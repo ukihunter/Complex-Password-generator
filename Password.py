cddb5943b76dba3e49e04efd209a00ce927e8e45
NikiJr94 <146814485+NikiJr94@users.noreply.github.com>
2024-08-23 13:12:22 +0530

    The code for the programme
    The programme generates a complex password of 12 digits and then it copies this password to your clipboard after that the password will be sent to your desired txt file

---
+++ Password.py
@@ -0,0 +1,44 @@
+import random
+import string
+import pyperclip
+Password_title = input("Password title? ")
+output = ""
+while True:
+    type = input('type? [a = all, n= nombers, l= letters] ')
+
+    if type == 'a':
+        for x in range(0,12):
+            x = random.choice(string.digits + string.ascii_letters + string.punctuation)
+            output += x
+        break
+
+    elif type == 'n':
+        for x in range(0, 12):
+            x = random.choice(string.digits)
+            output += x
+        break
+
+    elif type == 'l':
+        for x in range(0,12):
+            x = random.choice(string.ascii_letters)
+            output += x
+        break
+
+    else:
+        print('invalid command')
+
+file_path = r"C:\Users\techb\Desktop\Password.txt"
+print(output)
+
+
+try:
+    with open(file_path, 'a') as file:
+        file.write(f"{Password_title} = {output}\n")
+    pyperclip.copy(output)
+    print(f"Test file created successfully at {file_path}")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+
+
+'zTNBQ5eT);g
